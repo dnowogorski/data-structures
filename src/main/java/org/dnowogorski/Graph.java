8aeb445d630bfe05f5f0e39f44995e3845c231a4
dnowogorski <nowogorski.dawid@gmail.com>
2018-11-21 20:14:16 +0100

    Add initial adjacency list graph representation

---
+++ src/main/java/org/dnowogorski/Graph.java
@@ -0,0 +1,102 @@
+package org.dnowogorski;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Graph {
+
+    private final int V;
+    private final List<List<Integer>> adj;
+
+    /**
+     * Create an empty graph with V vertices
+     *
+     * @param V number of vertices
+     */
+    public Graph(int V) {
+        this.V = V;
+        adj = new ArrayList<>(V);
+        for (int i = 0; i < V; i++) adj.add(i, new LinkedList<>());
+    }
+
+    /**
+     * Add edge between two vertices
+     *
+     * @param v first vertex
+     * @param w second vertex
+     */
+    public void addEdge(int v, int w) {
+        adj.get(v).add(w);
+        if (v != w) adj.get(w).add(v);
+    }
+
+    /**
+     * Return collection of vertices adjacent to v
+     *
+     * @param v vertex
+     * @return collection of vertices
+     */
+    public Collection<Integer> adj(int v) {
+        return adj.get(v);
+    }
+
+    /**
+     * Return number of vertices
+     *
+     * @return number of edges
+     */
+    public int getNumberOfVertices() {
+        return V;
+    }
+
+    /**
+     * Return number of edges
+     *
+     * @return number of edges
+     */
+    public long getNumberOfEdges() {
+        return adj.stream()
+                .map(List::size)
+                .mapToInt(Integer::intValue)
+                .sum() / 2;
+    }
+
+    /**
+     * Compute the degree of given vertex
+     *
+     * @param v vertex
+     * @return degree
+     */
+    public int degree(int v) {
+        return adj.get(v).size();
+    }
+
+    /**
+     * Compute max degree in the graph
+     *
+     * @return max degree
+     */
+    public int maxDegree() {
+        return adj.stream()
+                .map(List::size)
+                .mapToInt(Integer::intValue)
+                .max()
+                .orElse(0);
+    }
+
+    /**
+     * Return number of self loops in the graph
+     *
+     * @return number of self loops
+     */
+    public int getNumberOfSelfLoops() {
+        int count = 0;
+        for (int i = 0; i < V; i++) {
+            for (int v : adj(i))
+                if (i == v) count++;
+        }
+        return count;
+    }
+}
---
+++ src/test/java/org/dnowogorski/GraphTest.java
@@ -0,0 +1,53 @@
+package org.dnowogorski;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+public class GraphTest {
+
+    private Graph graph;
+
+    @Before
+    public void createGraph() {
+        graph = new Graph(4);
+        graph.addEdge(0, 0);
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 2);
+        graph.addEdge(0, 3);
+        graph.addEdge(1, 1);
+        graph.addEdge(2, 3);
+    }
+
+    @Test
+    public void adj_returnsAdjacencyList() {
+        assertThat(graph.adj(0)).contains(0, 1, 2, 3);
+        assertThat(graph.adj(1)).contains(0, 1);
+        assertThat(graph.adj(2)).contains(0, 3);
+        assertThat(graph.adj(3)).contains(0, 2);
+    }
+
+    @Test
+    public void getNumberOfEdges_returnsEdgeCount() {
+        assertThat(graph.getNumberOfEdges()).isEqualTo(5);
+    }
+
+    @Test
+    public void degree_returnsVertexDegree() {
+        assertThat(graph.degree(0)).isEqualTo(4);
+        assertThat(graph.degree(1)).isEqualTo(2);
+        assertThat(graph.degree(2)).isEqualTo(2);
+        assertThat(graph.degree(3)).isEqualTo(2);
+    }
+
+    @Test
+    public void maxDegree_computesMaxDegree() {
+        assertThat(graph.maxDegree()).isEqualTo(4);
+    }
+
+    @Test
+    public void getNumberOfSelfLoops_returnsNumberOfSelfLoops() {
+        assertThat(graph.getNumberOfSelfLoops()).isEqualTo(2);
+    }
+}
